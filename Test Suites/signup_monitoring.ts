9e6d513d009b9a13b473cab922f5c952f0757ebc
juncarlolimoran <juncarlolimoran@localhost>
2019-11-28 12:41:50 +0800

    update

---
+++ Test Suites/signup_monitoring.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>signup_monitoring</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>01d50ced-cbe1-4c18-a62c-52deff4f7af1</testSuiteGuid>
+   <testCaseLink>
+      <guid>cc790d75-d02f-48b4-b6f5-ee60bff47c58</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/signup</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
